4bd272b27b73878865bdc826a6545deb1677dcce
danielpuliche <66581056+danielpuliche@users.noreply.github.com>
2022-05-06 03:07:04 -0500

    actualizar

--- settings/controller.php
+++ settings/controller.php
@@ -1,8 +1,4 @@
 <?php
-// Conexion a la base de datos
-include_once("../settings/db.php");
-$connection = mysqli_connect($host, $user, $pw, $db);
-mysqli_set_charset($connection, "utf8");
 
 // Controlador para agregar mensaje
 if (isset($_POST['agregar-mensaje'])){
